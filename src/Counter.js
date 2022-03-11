dcb61520df0de854237e54b77dedd66ffbca90c7
AyaneSHOJI <shojiayane@gmail.com>
2022-03-11 17:22:22 +0100

    Color change func added

--- src/Counter.js
+++ src/Counter.js
@@ -1,23 +1,46 @@
 import { useState } from 'react';
 import './Counter.css';
+import Button from 'react-bootstrap/Button';
 
 function Counter(){
-    const [count, setCount] = useState(0);
+    const [countValue, setCountValue] = useState(0);
+    const [bgColor, setBgColor] = useState("bg-color-green");
 
+    const increaseCount = () => {
+        if(countValue === 15) return;
+
+        const newCountValue = countValue + 1;
+
+        setCountValue(newCountValue);
+        if(newCountValue >= 13){
+            setBgColor("bg-color-red");
+        }
+    };
+
+    const decreaseCount = () => {
+        if(countValue === 0) return;
+
+        const newCountValue = countValue - 1;
+        
+        setCountValue(newCountValue);
+        if(newCountValue < 13){
+            setBgColor("bg-color-green");
+        }
+    }
+    
     return (
-        <div className="Counter">
-            <div>
-                <p id="number">{count}</p>
-            </div>
-            <div className="buttons">
-                <div className="button">
-                    <button onClick={() => setCount(count - 1)}>-</button>
+        <div className={bgColor}>
+            <div className="container">
+                <div className="row">
+                    <p className="">{countValue} voitures entrées</p>
                 </div>
-                <div className="button">
-                    <button onClick={() => setCount(count + 1)}>+</button>
+                <div className="btn-groupe row">
+                    <Button className="btn btn-primary my-3" onMouseDown={decreaseCount}>-</Button>             
+                    <Button className="btn btn-primary my-3" onClick={increaseCount}>+</Button>
                 </div>
             </div>
         </div>
+        
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -5,12 +6,15 @@ import App from './App';
 import Counter from './Counter';
 import reportWebVitals from './reportWebVitals';
 
+
+
 ReactDOM.render(
   <React.StrictMode>
     <Counter />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
