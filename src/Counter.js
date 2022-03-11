c9c26ce34976d3caa9e850913f1c09e86ca883b7
AyaneSHOJI <shojiayane@gmail.com>
2022-03-11 15:50:19 +0100

    add and reduce function added on buttons

--- src/Counter.js
+++ src/Counter.js
@@ -1,17 +1,20 @@
+import { useState } from 'react';
 import './Counter.css';
 
 function Counter(){
+    const [count, setCount] = useState(0);
+
     return (
         <div className="Counter">
             <div>
-                <p id="number">0</p>
+                <p id="number">{count}</p>
             </div>
             <div className="buttons">
                 <div className="button">
-                    <button>-</button>
+                    <button onClick={() => setCount(count - 1)}>-</button>
                 </div>
                 <div className="button">
-                    <button>+</button>
+                    <button onClick={() => setCount(count + 1)}>+</button>
                 </div>
             </div>
         </div>
