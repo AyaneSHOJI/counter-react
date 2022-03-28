a803f1882f0a768e1758d62c59bb055466c4003e
AyaneSHOJI <shojiayane@gmail.com>
2022-03-29 00:09:39 +0200

    updated settings

--- src/Counter.js
+++ src/Counter.js
@@ -1,27 +1,40 @@
 import { useState } from 'react';
 import './Counter.css';
 
-function Counter(){
+const Counter = () =>{
+    //Définir l'état de base
     const [countValue, setCountValue] = useState(0);
     const [bgColor, setBgColor] = useState("bg-color-green");
 
+    //Evenements avec le btn +
     const increaseCount = () => {
+        //Pas plus que 15
         if(countValue === 15) return;
 
+        //Ajouter 1 quand le btn est cliqué
         const newCountValue = countValue + 1;
 
+        //Assigner une nouvelle valeur
         setCountValue(newCountValue);
+
+        //Système d'alert, changer de couleur
         if(newCountValue >= 13){
             setBgColor("bg-color-red");
         }
     };
 
+    //Evenements avec le btn -
     const decreaseCount = () => {
+        //Pas moins que 0
         if(countValue === 0) return;
 
+        //Reduire 1 quand le btn est cliqué
         const newCountValue = countValue - 1;
         
+        //Assigner une nouvelle valeur
         setCountValue(newCountValue);
+
+        //Système d'alert, changer de couleur
         if(newCountValue < 13){
             setBgColor("bg-color-green");
         }
@@ -29,11 +42,14 @@ function Counter(){
     
     return (
         <div className="container">
+            {/* div où la couleur change, le nom de className avec une interpolation */}
             <div className={`counterContainer  ${bgColor}`}>
                 <div className="countValueDiv">
+                    {/* Afficher le nombre */}
                     <h1 className="countValue">{countValue}</h1>
                 </div>
-                <div className="btn-group">                   
+                <div className="btn-group">
+                    {/* Relier les actions de btn avec onClick() */}
                     <button className="btn btn-primary my-3" onClick={decreaseCount}>-</button>       
                     <button className="btn btn-primary my-3" onClick={increaseCount}>+</button>     
                 </div>
---
+++ src/Counter2.js
@@ -0,0 +1,1 @@
+// Ajouter la version avec class
